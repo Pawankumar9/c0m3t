e7178f84ab58c94d0b4fcf7ba7ab6b95697f393d
JunTae Son <sonjt0705@outlook.com>
2017-04-18 20:10:07 +0900

    Update rcopy.py
    Finished!

--- Reverse-Copy/rcopy.py
+++ Reverse-Copy/rcopy.py
@@ -1,9 +1,26 @@
+# Program Banner
 print('Reverse Copy')
 print('Forensics')
 
+# File Name Input
 print('Source File Name')
 srcFileName = input()
 print('Result File Name')
 rstFileName = input()
 
-sf = open(srcFileName, 'r')
+# Reading Source File
+print('Read Source File...')
+srcf = open(srcFileName, 'rb')
+sfdata = srcf.read()
+srcf.close()
+
+# Reverse Data
+print('Reverse File Data...')
+rfdata = sfdata[::-1]
+
+# Writing Result File
+print('Write Result File...')
+rstf = open(rstFileName, 'wb')
+rstf.write(rfdata)
+rstf.close()
+print('Finished!')
