e60838adc50975b35cf8eb93786ceef5f7c4ad6b
JunTae Son <sonjt0705@outlook.com>
2017-06-03 23:03:08 +0900

    Update Command Shell Stealer
    Change annotation.

--- Command-Shell-Stealer/cshs.py
+++ Command-Shell-Stealer/cshs.py
@@ -10,19 +10,19 @@ import telnetlib
 def connect(address, port):
     return socket.create_connection((address, port))
 
-# Packing on x86 platform
+# Packing data from x86 platform
 def x86pack(data):
     return struct.pack('<I', data)
 
-# Unpacking on x86 platform
+# Unpacking data from x86 platform
 def x86unpack(data):
     return struct.unpack('<I', data)[0]
 
-# Packing on x64 platform
+# Packing data from x64 platform
 def x64pack(data):
     return struct.pack('<Q', data)
 
-# Unpacking on x64 platform
+# Unpacking data from x64 platform
 def x64unpack(data):
     return struct.unpack('<Q', data)[0]
 
