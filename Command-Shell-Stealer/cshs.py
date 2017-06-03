27a7aad9d5ddd6a157c606af41ce3120ca2594d6
JunTae Son <sonjt0705@outlook.com>
2017-06-03 19:43:39 +0900

    Update Command Shell Stealer
    Add x86 platform pack function and x86 platform unpack function.

--- Command-Shell-Stealer/cshs.py
+++ Command-Shell-Stealer/cshs.py
@@ -5,9 +5,17 @@ import os
 import struct
 import telnetlib
 
-# Connect to target server
+# Target server connection
 def connect(address, port):
     return socket.create_connection((address, port))
+
+# Packing on x86 platform
+def x86pack(data):
+    return struct.pack('<I', data)
+
+# Unpacking on x86 platform
+def x86unpack(data):
+    return struct.unpack('<I', data)[0]
 
 # Program Banner
 print('Command Shell Stealer')
