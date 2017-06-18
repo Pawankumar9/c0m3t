0767766c04bfaab95dd64d052fa07de7d896cf2b
JunTae Son <sonjt0705@outlook.com>
2017-06-19 00:19:46 +0900

    Update Surface Analyzer
    Add program banner.

--- Surface-Analyzer/sfaz.py
+++ Surface-Analyzer/sfaz.py
@@ -1,3 +1,7 @@
 # Python 3.5.2 or 3.6.1 or later is required!
 # Windows Sysinternals Suite or GNU Binutils is required!
 import sys
+
+# Program Banner
+print('Surface Analyzer Nu')
+print('Reverse Engineering')
